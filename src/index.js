03d3216d7f6f7b8496090aea29e33f2df6759c68
Alonso Anselmo Gómez Sánchez <68868495+AAGS589@users.noreply.github.com>
2024-07-02 12:26:12 -0600

    Se agrego funcion de suma combinada

--- src/index.js
+++ src/index.js
@@ -5,11 +5,37 @@
  * @param {number[]} currentCombination - currently explored candidates.
  * @param {number} startFrom - index of the candidate to start further exploration from.
  * @return {number[][]}
+ * @return {void}
  */
 
 const combinationSumRecursive = (
-    
+  candidates,
+  remainingSum,
+  finalCombinations,
+  currentCombination,
+  startFrom
+) => {
+  // Si la suma restante es 0, añadimos una copia de la combinación actual a las combinaciones finales
+  if (remainingSum === 0) {
+    finalCombinations.push([...currentCombination]);
+    return;
   }
+
+  // Iteramos sobre los candidatos comenzando desde startFrom
+  for (let i = startFrom; i < candidates.length; i++) {
+    if (candidates[i] <= remainingSum) {
+      currentCombination.push(candidates[i]);
+      combinationSumRecursive(
+        candidates,
+        remainingSum - candidates[i],
+        finalCombinations,
+        currentCombination,
+        i
+      );
+      currentCombination.pop();
+    }
+  }
+};
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
@@ -19,7 +45,9 @@ const combinationSumRecursive = (
    * @return {number[][]}
    */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
-  }
+    const finalCombinations = []
+    combinationSumRecursive(candidates, target, finalCombinations, [], 0);
+    return finalCombinations;
+  };
 
 module.exports = combinationSum;
